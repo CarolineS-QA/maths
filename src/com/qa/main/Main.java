7b094bf86fc9031c6d7ba35e1020cf232707bea0
Chas3-Mahat <56127094+Chas3-Mahat@users.noreply.github.com>
2020-03-23 16:44:40 +0000

    added multiple, minus and divide methods to test

--- src/com/qa/main/Main.java
+++ src/com/qa/main/Main.java
@@ -5,6 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		Maths maths = new Maths();
 		System.out.println(maths.addThings());
+		System.out.println(maths.multiplyThings());
+		System.out.println(maths.minusThings());
+		System.out.println(maths.divideThings());
 	}
 
 }
--- src/com/qa/main/Maths.java
+++ src/com/qa/main/Maths.java
@@ -1,9 +1,28 @@
 package com.qa.main;
 
 public class Maths {
+	
 	public int addThings() {
 	int numberOne = 2;
 	int numberTwo = 3;
 	return numberOne + numberTwo;
+	}
+	
+	public int multiplyThings() {
+	int numberOne = 2;
+	int numberTwo = 3;
+	return numberOne * numberTwo;
+	}
+	
+	public int minusThings() {
+	int numberOne = 2;
+	int numberTwo = 3;
+	return numberOne - numberTwo;
+	}
+	
+	public int divideThings() {
+	int numberOne = 2;
+	int numberTwo = 4;
+	return numberOne / numberTwo;
 	}
 }
