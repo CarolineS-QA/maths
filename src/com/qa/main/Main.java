8b47ba6315d75cd3e9bdc371e4adfd4fb1680fc0
Chas3-Mahat <56127094+Chas3-Mahat@users.noreply.github.com>
2020-03-23 16:59:22 +0000

    changed divide to double, and change to addition method

--- src/com/qa/main/Main.java
+++ src/com/qa/main/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Maths maths = new Maths();
-		System.out.println(maths.addThings());
+		System.out.println(maths.addThings(2, 3));
 		System.out.println(maths.multiplyThings());
 		System.out.println(maths.minusThings());
 		System.out.println(maths.divideThings());
--- src/com/qa/main/Maths.java
+++ src/com/qa/main/Maths.java
@@ -2,10 +2,8 @@ package com.qa.main;
 
 public class Maths {
 	
-	public int addThings() {
-	int numberOne = 2;
-	int numberTwo = 3;
-	return numberOne + numberTwo;
+	public int addThings(int num1, int num2) {
+		return num1 + num2;
 	}
 	
 	public int multiplyThings() {
@@ -20,9 +18,9 @@ public class Maths {
 	return numberOne - numberTwo;
 	}
 	
-	public int divideThings() {
-	int numberOne = 2;
-	int numberTwo = 4;
+	public double divideThings() {
+	double numberOne = 2;
+	double numberTwo = 4;
 	return numberOne / numberTwo;
 	}
 }
