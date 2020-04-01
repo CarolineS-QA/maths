5c78266058f82446cd779a2f0547d985a69efa5f
Chas3-Mahat <56127094+Chas3-Mahat@users.noreply.github.com>
2020-04-01 12:23:32 +0100

    Division Exceptions Exercise

--- src/com/qa/main/Main.java
+++ src/com/qa/main/Main.java
@@ -4,9 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		Maths maths = new Maths();
-		System.out.println(maths.addThings(2, 3));
-		System.out.println(maths.multiplyThings());
-		System.out.println(maths.minusThings());
+//		System.out.println(maths.addThings(2, 3));
+//		System.out.println(maths.multiplyThings(2, 3));
+//		System.out.println(maths.minusThings(2, 3));
+
+		// Exceptions focusing on the division method.
+		// Division method is designed to only work if the first number is smaller than
+		// the second.
 		System.out.println(maths.divideThings());
 	}
 
